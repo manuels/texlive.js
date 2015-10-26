286bdc06ef30d0a4b19d1796550a7990759d7ee4
Felix Zimmermann <fzimmermann89@gmail.com>
2015-10-27 00:42:05 +0100

    Change executable filename (in virtual filesystem) to ./latex
    and remove unneccessary additions to filesystem

--- pdftex.js
+++ pdftex.js
@@ -149,12 +149,8 @@ var PDFTeX = function(opt_workerPath) {
       ];
     else
       commands = [
-        curry(self, 'FS_createFolder', ['/', 'bin', true, true]),
-        curry(self, 'FS_createDataFile', ['/bin', 'this.program', '', true, true]),
         curry(self, 'FS_createDataFile', ['/', 'input.tex', source_code, true, true]),
         curry(self, 'FS_createLazyFile', ['/', 'latex.fmt', 'latex.fmt', true, true]),
-        curry(self, 'FS_createFolder', ['/bin/', 'share', true, true]),
-        curry(self, 'FS_createLazyFile', ['/bin/', 'texmf.cnf', './texlive/texmf-dist/web2c/texmf.cnf', true, true]),
         curry(self, 'FS_createLazyFilesFromList', ['/', 'texlive.lst', './texlive', true, true]),
       ];
 
--- post.js
+++ post.js
@@ -1,2 +1,5 @@
 self['postMessage'](JSON.stringify({'command': 'ready'}));
-shouldRunNow = true;
+//shouldRunNow = true;
+Module['calledRun']=false;
+Module['thisProgram']='/latex';
+FS.createDataFile("/",Module['thisProgram'],"dummy for kpathsea",true,true);
